90e98c0d6db16dc6fb47c6278a767c8401d218e3
Werisson <werissonfelix10@gmail.com>
2026-04-19 16:48:59 -0300

    Finalizando o sistema de acerto, aplicando effects no text rating da note

--- Game/GameManager/GameManager.py
+++ Game/GameManager/GameManager.py
@@ -29,13 +29,15 @@ class GameManager:
         clock = pygame.time.Clock()
         score = 0
         notesManage = NoteManager(self.config.get_note_width(), self.config.get_note_height(),(200,0,0),self.config.get_base_speed())
+        alpha = 0
+        current_rating = ""
         while self.running:
             dt = clock.tick(60) / 1000
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
-
+                    
             self.screen.fill((0,0,0))
             keys_pressed = pygame.key.get_pressed()
             current_time = mixer.music.get_pos() / 1000
@@ -47,12 +49,34 @@ class GameManager:
                 else:
                     key.draw_line()
                    
-            score = notesManage.while_running(score, current_time,notes,self.config.get_spawn_offset(),self.screen,self.default_lane, keys_pressed)
-
-            score_text = self.font.render(f"Score: {score}", True, (255,255,255))
-                    
+            score, rating = notesManage.while_running(score, current_time,notes,self.config.get_spawn_offset(),self.screen,self.default_lane, keys_pressed)
+            #orig_surf = self.font.render(rating, True, (255, 255, 255))
+            #text_surf = orig_surf.copy()   
+                        
+            #alpha_surf = pygame.Surface(text_surf.get_size(), pygame.SRCALPHA)
+            #text_surf = orig_surf.copy() 
+            
+            notesManage.draw_text(rating,self.screen)
+            notesManage.effect_text_rating()
+            """
+            if rating != "":
+                current_rating = rating
+                alpha = 255
+            
+            if alpha > 0 and current_rating != "":
+                orig_surf = self.font.render(current_rating, True, (255, 255, 255))
+                temp_surf = pygame.Surface(orig_surf.get_size(), pygame.SRCALPHA)
+                
+                temp_surf.set_alpha(alpha)   
+                temp_surf.blit(orig_surf, (10,300))
+                                                               
+                self.screen.blit(temp_surf, (10,300))  
+                alpha = max(alpha-4, 0)
+            """
+                     
+            score_text = self.font.render(f"Score: {score}", True, (255,255,255))     
             self.screen.blit(score_text, (10, 10))
             pygame.display.update()
-
+        
 testee = GameManager()
 testee.run()
--- Game/Notes/NotesManager.py
+++ Game/Notes/NotesManager.py
@@ -3,7 +3,6 @@ from Game.Config.Game_Config import GameConfig
 
 class NoteManager:
     def __init__(self,  width, height, color,speed):
-
         self.width = width
         self.height = height
         self.color = color
@@ -11,11 +10,17 @@ class NoteManager:
         self.speed = speed
         self.font = pygame.font.Font(None, 36)
         self.notes_to_remove = []
+        
         self.rating = ""
+        self.current_rating = ""
+        
+        self.alpha = 0
+        self.col_spd = 5
+        self.col_dir = [-1,-1,-1]
+        self.def_col = [255,255,255]
         
         self.x = 0
-        self.y = 0
-        
+        self.y = 0    
     def while_running(self,score, current_time, notes, spawn_offset, screen, keys, keys_pressed):            
         for note in notes:
             note_time, lane = note
@@ -31,8 +36,8 @@ class NoteManager:
                 pygame.draw.rect(screen, (200,0,0),rect)
 
                 if rect.colliderect(keys[lane].rect):
-                    if keys_pressed[keys[lane].key]:
-                                           
+                    if keys_pressed[keys[lane].key]: 
+                                                              
                         distance = abs(rect.centery - keys[lane].rect.centery)
                         
                         if distance <= 12:                          
@@ -41,30 +46,40 @@ class NoteManager:
                         elif 13 <= distance <= 18:
                             self.rating = "Good"
                             score += 150
-                        else:
+                        elif distance >= 19:
                             self.rating = "Bad"
-                            score += 50
-                        
-                        print(distance, self.rating, score)                
-                        rating_text = self.font.render(self.rating, True, (255,255,255))
-                                                            
-                        screen.blit(rating_text, (10, 300))
-                                               
+                            score += 50                          
                         self.notes_to_remove.append(note)
                 elif self.y > 600:
-                    rating = "Miss"
-                    rating_text = self.font.render(rating, True, (255,255,255))
-         
-                    screen.blit(rating_text, (10, 300))
+                    self.rating = "Miss" 
                     self.notes_to_remove.append(note)
-
         for n in self.notes_to_remove:
             if n in notes:
                 notes.remove(n)
-        return score
+        return score, self.rating
     
-    def draw(self, screen):
-        rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        pygame.draw.rect(screen,self.color, rect)
-    
-    
+    def draw_text(self,rating, screen):
+        if rating != "":
+            self.current_rating = rating
+            self.alpha = 255
+        if self.alpha > 0 and self.current_rating != "":
+            orig_surf = self.font.render(self.current_rating, True, self.def_col)
+            temp_surf = pygame.Surface(orig_surf.get_size(), pygame.SRCALPHA)
+            
+            temp_surf.set_alpha(self.alpha)
+            temp_surf.blit(orig_surf, (0,0))
+            
+            screen.blit(temp_surf, (10,300))
+            self.alpha = max(self.alpha - 4, 0)  
+    def effect_text_rating(self):
+        mininum = 100
+        maximum = 200
+        
+        for i in range(3):
+            self.def_col[i] += self.col_spd * self.col_dir[i]
+            
+            if self.def_col[i] >= 255:
+                col_dir = 0
+            elif self.def_col[i] <= 0:
+                self.def_col[i] = 255
+        

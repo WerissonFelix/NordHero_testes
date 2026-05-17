999a077c4e1bd48455df4f956c8a685453da71a4
Werisson <werissonfelix10@gmail.com>
2026-05-17 14:03:18 -0300

    feat: Iniciando o ScoreRepository, ele vai gerenciar tudo relacionado as scores, seja de apenas um jogador em relação a determinadas músicas ou dificuldades, seja para todos os jogadores ao mesmo tempo. Primeiro método para apenas um jogador foi finalizado, filtro com base no id do user

---
+++ DataBase/repositories/score_repository.py
@@ -0,0 +1,54 @@
+from DataBase.repositories.base_repository import BaseRepository
+from models.score import Score
+
+class ScoreRepository(BaseRepository):
+    
+    def create(self, score: Score):
+        query = """
+        insert into scores 
+        (user_id, chart_id, score, accuracy, rank)
+        values (?, ?, ?, ?, ?)
+        """
+        
+        self.execute(query, (score.user_id, score.chart_id, score.score, score.accuracy, score.rank))
+    
+    
+    def delete(self, score_id):
+        query = """ 
+        delete from scores 
+        where id = ?
+        """
+        
+        self.execute(query, (score_id, ))    
+    """ 
+    ===========================================================
+            FUNÇÕES RELACIONADOS PARA UM USER (UM JOGADOR)
+    ===========================================================
+    """
+    def get_by_user_id(self, user_id):
+        """ 
+        Manda a query de busca para manipular todos os scores de todas as músicas/charts.
+        Muito importante para fazer um histórico pessoal, ou até mesmo um gráfico de progressão
+        de habilidades. (possível feature??????)
+        
+        Retorna uma lista, onde cada elemento é o tipo Score, da class models.
+        
+        """
+        query = """
+        select * from scores
+        where user_id = ?
+        """
+        
+        rows = self.fetchall(query, (user_id, ))
+        
+        all_scores = []
+        
+        if rows is None:
+            return None
+        
+        for row in rows:
+            all_scores.append(Score(row[0],row[1], row[2], row[3], row[4], row[5]))
+        return all_scores
+
+
+        

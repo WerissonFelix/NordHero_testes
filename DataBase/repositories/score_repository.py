edfb29589e5b46766f8dfed5408a40a8da23dd62
Werisson <werissonfelix10@gmail.com>
2026-05-17 14:34:11 -0300

    feat: Apenas salvando a ideia que tive para resolver o problema do método get_by_user_difficulty, talvez seja necessário usar inner join

--- DataBase/repositories/score_repository.py
+++ DataBase/repositories/score_repository.py
@@ -12,7 +12,6 @@ class ScoreRepository(BaseRepository):
         
         self.execute(query, (score.user_id, score.chart_id, score.score, score.accuracy, score.rank))
     
-    
     def delete(self, score_id):
         query = """ 
         delete from scores 
@@ -25,7 +24,7 @@ class ScoreRepository(BaseRepository):
             FUNÇÕES RELACIONADOS PARA UM USER (UM JOGADOR)
     ===========================================================
     """
-    def get_by_user_id(self, user_id):
+    def get_all_by_user_id(self, user_id):
         """ 
         Manda a query de busca para manipular todos os scores de todas as músicas/charts.
         Muito importante para fazer um histórico pessoal, ou até mesmo um gráfico de progressão
@@ -50,5 +49,29 @@ class ScoreRepository(BaseRepository):
             all_scores.append(Score(row[0],row[1], row[2], row[3], row[4], row[5]))
         return all_scores
 
-
+    def get_by_user_chart_id(self, user_id, chart_id):
+        query = """
+        select * from scores
+        where (user_id = ? and song_id = ?)
+        """
         
+        rows = self.fetchall(query, (user_id, chart_id))
+        
+        if rows is None:
+            return None
+        
+        all_scores = []
+        
+        for row in rows:
+            all_scores.append(Score(row[0],row[1], row[2], row[3], row[4], row[5]))
+        return all_scores
+    
+    def get_by_user_difficulty(self, user_id, difficulty):
+        
+        """     
+        
+        Acho que aqui será necessário usar um inner join, porque a tabela score não tem difficulty id, mas a tabela 
+        song_charts tem difficulty id, e scores tem chart id, ou sejaaa, inner join provávelmente vai resolver, mas 
+        preciso revisar isso antes.
+        
+        """

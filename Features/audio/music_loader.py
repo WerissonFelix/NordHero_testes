951314428e31dc8fe74b6064251961c986f43a6c
Werisson <werissonfelix10@gmail.com>
2026-05-25 20:38:31 -0300

    feat: Update music loader to include music type in difficulty mapping and refactor loading logic

--- Features/audio/music_loader.py
+++ Features/audio/music_loader.py
@@ -15,25 +15,23 @@ list_musics = []
 
 story_difficulty_map = {
 
-
-    "Cafuné - tek it (instrumental)": 1,
-    "Debussy - Clair de Lune - Rousseau (youtube)": 1,
-    "Die With A Smile (Instrumental) - Lady Gaga": 1,
-    "Imagine Dragons   Believer Official Instrumental - Anything And Everything (youtube)": 1,
-    "Like Him (Instrumental) - Tyler the Creator - pb (abandoned) (youtube)": 1,
-    "Ludovico Einaudi - Una Mattina (The Intouchables) - Rousseau (youtube)": 1,
-
+    "Cafuné - tek it (instrumental)": (1, "Single Player"),
+    "Debussy - Clair de Lune - Rousseau (youtube)": (1, "Single Player"),
+    "Die With A Smile (Instrumental) - Lady Gaga": (1, "Single Player"),
+    "Imagine Dragons   Believer Official Instrumental - Anything And Everything (youtube)": (1, "Single Player"),
+    "Like Him (Instrumental) - Tyler the Creator - pb (abandoned) (youtube)": (1, "Single Player"),
+    "Ludovico Einaudi - Una Mattina (The Intouchables) - Rousseau (youtube)": (1, "Single Player"),
     
-    "I Thought I Saw Your Face Today - She & Him (Instrumental)": 2,
-    "Fallen Down (Reprise) - Toby Fox (youtube)": 2,
-    "Another One Bites The Dust - Instrumental": 2,
-    "I Thought I Saw Your Face Today (FULL)": 2,
-    "She & Him - I Thought I Saw Your Face Today (Acapella_Vocals Only)": 2,
-    "Michael Jackson  Billie Jean [Instrumental Version] - HIStoryWorldTourMJ (youtube)": 2,
+    "I Thought I Saw Your Face Today - She & Him (Instrumental)": (2, "Single Player"),
+    "Fallen Down (Reprise) - Toby Fox (youtube)": (2, "Single Player"),
+    "Another One Bites The Dust - Instrumental": (2, "Single Player"),
+    "I Thought I Saw Your Face Today (FULL)": (2, "2 Player"),
+    "She & Him - I Thought I Saw Your Face Today (Acapella_Vocals Only)": (2, "Single Player"),
+    "Michael Jackson  Billie Jean [Instrumental Version] - HIStoryWorldTourMJ (youtube)": (2, "Single Player"),
 
-    "Abolish the IRS": 3,
-    "Ana Vidovic - Asturias by Isaac Albéniz -": 3,
-    "MEGALOVANIA - Toby Fox": 3,  
+    "Abolish the IRS": (3, "Single Player"),
+    "Ana Vidovic - Asturias by Isaac Albéniz -": (3, "Single Player"),
+    "MEGALOVANIA - Toby Fox": (3, "Single Player"),
   
 }
 
@@ -42,19 +40,24 @@ def load_and_register_music():
     for entry in file_path.iterdir():
         if entry.suffix == ".mp3":
             
-            story_difficulty_id = story_difficulty_map[entry.stem]               
+            music_info = story_difficulty_map[entry.stem]               
+            
+            story_difficulty_id = music_info[0]
+            music_type = music_info[1]
+            
             music = mixer.Sound(str(entry))
             list_musics.append(Song(
                 None,
                 entry.stem,
-                " ",
+                music_type,
                 0.0,
                 int(music.get_length()),
                 str(entry),
                 story_difficulty_id 
-       
             ))
+            
     songManager = SongRepository()
+    
     for music in list_musics:
         song_exists = songManager.get_by_title(music.title)
         
@@ -62,3 +65,5 @@ def load_and_register_music():
             continue
         else:
             songManager.create(music)
+
+load_and_register_music()
